7559e7a68b7b3e8d4c876880be78828f75654f79
Ozhoh, Oleksandr <oleksandrozhoh@gmail.com>
2024-02-11 13:08:58 -0600

    Async await functions

---
+++ AsyncAwaitFunctions.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const countriesContainer = document.querySelector('.countries');
+
+function renderCountry(countryData, className = '') {
+  const html = `<div class="countries">
+  <article class="country ${className}">
+    <img class="country__img" src="${countryData.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${countryData.name.common}</h3>
+      <h4 class="country__region">${countryData.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +countryData.population / 1000000
+      ).toFixed(1)} Mil</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.values(countryData.languages)[0]
+      }</p>
+      <p class="country__row"><span>💰</span>${[
+        Object.keys(countryData.currencies),
+      ]}</p>
+    </div>
+  </article>
+</div>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+}
+
+const whereAmI = async function (country) {
+  try {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/name/${country}`
+    );
+    const data = await response.json();
+    console.log(data[0]);
+    renderCountry(data[0]);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+whereAmI('usa');
